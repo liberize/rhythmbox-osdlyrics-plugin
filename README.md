349e9a821f3a17fc2ffc8d925a7aae6943089e4a
liberize <liberize@gmail.com>
2013-06-05 12:18:00 +0800

    original

---
+++ README.md
@@ -0,0 +1,30 @@
+# Rhythmbox Osdlyrics Plugin
+
+This is a simple plugin that helps you to toggle lyrics display through toolbar button. 
+It calls [osdlyrics](https://code.google.com/p/osd-lyrics/) to display lyrics, so osdlyrics is required.
+
+## Introduction
+
+This plugin will create a toolbar button, like this:
+
+![Toolbar Button](http://liberize.github.io/images/rhythmbox-osdlyrics-plugin.jpg)
+
+You can toggle lyrics display by clicking it.
+
+Besides, it will remember your lyrics state (on/off) every time you quit rhythmbox.
+
+## Install
+
+Use `git clone` to download or simply download as zip file.
+
+Just copy this directory to `~/.local/share/rhythmbox/plugins/` (if not exist, make one).
+
+Then open rhythmbox, enable it in plugin manager dialog.
+
+_PS: you may want to disable the original lyrics plugin as well._
+
+## Contact
+
+* @liberize: <https://github.com/liberize>
+* Blog Page: <http://liberize.github.io/pages/projects/rhythmbox-osdlyrics-plugin.html>
+* Email: <liberize@gmail.com>
---
+++ showlyrics.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+import os
+from gi.repository import Gtk, Gdk, GObject, Pango, Peas
+from gi.repository import RB
+from ConfigParser import ConfigParser
+
+ui_str = """
+<ui>
+  <toolbar name="ToolBar">
+    <toolitem name="ShowLyrics" action="ShowLyrics"/>
+    <separator/>
+  </toolbar>
+</ui>
+"""
+cur_path = os.path.dirname(os.path.realpath(__file__))
+conf_file = cur_path + '/config'
+config = ConfigParser()
+lyrics_on = True
+
+class ShowLyricsPlugin(GObject.Object, Peas.Activatable):
+	__gtype_name__ = 'ShowLyricsPlugin'
+
+	object = GObject.property (type = GObject.Object)
+
+	def __init__(self):
+		GObject.Object.__init__(self)
+		self.window = None
+			
+	def do_activate(self):
+		global config, lyrics_on
+		
+		data = dict()
+		shell = self.object
+		manager = shell.props.ui_manager
+		
+		config.read(conf_file)
+		lyrics_on = config.getboolean('Lyrics', 'Show')
+		if lyrics_on:
+			os.system('[ ! -n "`pgrep osdlyrics`" ] && osdlyrics&')
+			label = _("关闭歌词")
+		else:
+			label = _("显示歌词")
+
+		data['action_group'] = Gtk.ActionGroup(name='ShowLyricsPluginActions')
+		action = Gtk.Action(	name = 'ShowLyrics',
+					label = label,
+		                    	tooltip = _("显示/关闭歌词"),
+		                    	stock_id = Gtk.STOCK_EDIT
+		                    )
+		action.connect('activate', self.show_lyrics, shell)
+		data['action_group'].add_action(action)
+				
+		manager.insert_action_group(data['action_group'], 0)
+		data['ui_id'] = manager.add_ui_from_string(ui_str)
+		manager.ensure_update()
+
+		shell.data = data
+		
+		print "Plugin Activated."
+	
+	def do_deactivate(self):
+		global config, lyrics_on
+		
+		shell = self.object
+		data = shell.data
+
+		manager = shell.props.ui_manager
+		manager.remove_ui(data['ui_id'])
+		manager.remove_action_group(data['action_group'])
+		manager.ensure_update()
+
+		shell.data = None
+		
+		if self.window is not None:
+			self.window.destroy()
+		
+		config.set('Lyrics', 'Show', str(lyrics_on))
+		config.write(open(conf_file, 'w'))
+		os.system('[ -n "`pgrep osdlyrics`" ] && killall osdlyrics')
+
+		print "Plugin Deactivated."
+
+	def show_lyrics(self, action, shell):
+		global lyrics_on
+						
+		if lyrics_on:
+			os.system('[ -n "`pgrep osdlyrics`" ] && killall osdlyrics')
+			action.set_label(_("显示歌词"))
+			lyrics_on = False
+		else:
+			os.system('[ ! -n "`pgrep osdlyrics`" ] && osdlyrics&')
+			action.set_label(_("关闭歌词"))
+			lyrics_on = True
+		
+		manager = shell.props.ui_manager
+		manager.ensure_update()
